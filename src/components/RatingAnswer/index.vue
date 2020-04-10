83f7120cd03cf696f4c5a6c337e06aab093c0da0
Hugocaminada <Hugocaminada@users.noreply.github.com>
2020-04-10 16:32:38 +0200

    Made a gritview for the ranking photos

---
+++ src/components/RatingAnswer/index.vue
@@ -0,0 +1,36 @@
+<template>
+    <div class="col-3 answer py-3">
+        <img :src="image" alt="Card image cap" class="card-img img-home">
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "RatingAnswer",
+        props: {
+            image: {
+                type: String,
+                default: '',
+            }
+        },
+
+        data() {
+            return {
+                render: null,
+            }
+        },
+    }
+</script>
+
+<style lang="scss">
+    $component: 'answer';
+
+    .#{$component} {
+
+        .img-home {
+            height: 200px;
+            -o-object-fit: cover;
+            object-fit: cover;
+        }
+    }
+</style>
--- src/pages/RatePage/index.vue
+++ src/pages/RatePage/index.vue
@@ -1,17 +1,28 @@
 <template>
-    <div>
+    <div class="rate-page">
         <div>{{questions[currentQuestion]}}</div>
-
-
+        <div class="row">
+            <rating-answer v-for="(profile, index) in profiles"  :image="profile.image" :key="index" />
+        </div>
         <button type="button" class="btn btn-primary" :disabled="buttonDisabled" @click="nextQuestion">Primary</button>
-
     </div>
 </template>
 
 <script>
 
+    import {mapActions, mapGetters} from "vuex";
+    import RatingAnswer from '@/components/RatingAnswer'
+
     export default {
-        name: "rating",
+        name: "Rating",
+        components: {
+            RatingAnswer
+        },
+        computed: {
+            ...mapGetters({
+                profiles: 'profiles/list',
+            })
+        },
 
         data() {
             return {
@@ -36,11 +47,22 @@
         methods: {
             nextQuestion() {
                 this.currentQuestion ++
-            }
+            },
+            ...mapActions({
+                getProfiles: 'profiles/getProfiles',
+            })
+        },
+        mounted () {
+            this.getProfiles()
         }
     }
 </script>
 
-<style scoped>
+<style lang="scss">
+    $component: 'rate-page';
+
+    .#{$component} {
+        text-align: center;
+    }
 
 </style>
