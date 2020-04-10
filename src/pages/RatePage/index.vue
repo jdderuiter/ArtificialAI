f4d5ce467d51cd68fd35de425ea2870bfb035fa8
Hugocaminada <Hugocaminada@users.noreply.github.com>
2020-04-10 13:23:32 +0200

    Added some dummy questions and a button to go through them.

--- src/pages/RatePage/index.vue
+++ src/pages/RatePage/index.vue
@@ -1,10 +1,43 @@
 <template>
-    <div>Test</div>
+    <div>
+        <div>{{questions[currentQuestion]}}</div>
+
+
+        <button type="button" class="btn btn-primary" :disabled="buttonDisabled" @click="nextQuestion">Primary</button>
+
+    </div>
 </template>
 
 <script>
+
     export default {
-        name: "index"
+        name: "rating",
+
+        data() {
+            return {
+                currentQuestion: 0,
+                questions: [
+                    'Wie vind je het mooist?',
+                    'Wie houdt het meest van popmuziek?',
+                    'Wie is er single?'
+                ],
+                buttonDisabled: false,
+            }
+        },
+
+        watch: {
+            currentQuestion: function () {
+                if(this.currentQuestion === (this.questions.length - 1)) {
+                    this.buttonDisabled = true
+                }
+            }
+        },
+
+        methods: {
+            nextQuestion() {
+                this.currentQuestion ++
+            }
+        }
     }
 </script>
 
